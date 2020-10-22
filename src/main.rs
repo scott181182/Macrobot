f5ef4ef53209593885c40fb52c3b309a0c19f828
Nolan Rosen <nolanrosen15@gmail.com>
2020-10-21 23:23:57 -0400

    Parsing and executing keyboard inputs from WS text payloads

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ extern crate log;
 use actix_files::Files;
 use actix_web::{ get, web, App, HttpServer, Responder };
 
+const HOST: &str = "127.0.0.1";
 const PORT: u16 = 8073;
 
 mod websocket;
@@ -13,14 +14,17 @@ use websocket::init_ws;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_server=info,actix_web=info");
-    env_logger::init();
+    env_logger::Builder::new()
+        .default_format()
+        .filter_level(log::LevelFilter::Debug)
+        .init();
 
     HttpServer::new(|| {
         App::new()
             .service(init_ws)
             .service(Files::new("/", "./website/public").index_file("index.html"))
         })
-        .bind(format!("127.0.0.1:{}", PORT))?
+        .bind(format!("{}:{}", HOST, PORT))?
         .run()
         .await
 }
